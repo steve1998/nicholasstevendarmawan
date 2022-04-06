6a1120c60550f73b82b45ea7bd3e32fa450b6db8
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2022-04-05 21:51:17 -0700

    Add Avatar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,30 @@
 import Button from "./shared/Button";
 import links from "./shared/links.json";
+import ProfilePic from "../assets/profile_pic.jpg";
 
 function Hero() {
   return (
     <div className="flex flex-col h-3/4">
-      <div className="flex flex-col space-y-2 mb-4 tracking-tighter">
-        <span className="font-extrabold text-xl lg:text-2xl bg-clip-text text-transparent gradient-teal-to-sky">
-          Hi, I'm
-        </span>
-        <span className="font-extrabold text-3xl lg:text-8xl bg-clip-text text-transparent gradient-teal-to-sky">
-          Nicholas Steven Darmawan.
-        </span>
-        <span className="font-firacode font-regular text-lg lg:text-xl lg:text-2xl text-slate-500">
-          I am a Software Engineer with a passion for building things that help
-          the world work.
-        </span>
+      <div className="flex flex-row justify-between items-center">
+        <div className="md:w-1/2 flex flex-col space-y-2 mb-4 tracking-tighter">
+          <span className="font-extrabold text-xl lg:text-2xl bg-clip-text text-transparent gradient-teal-to-sky">
+            Hi, I'm
+          </span>
+          <span className="font-extrabold text-3xl lg:text-8xl bg-clip-text text-transparent gradient-teal-to-sky">
+            Nicholas Steven Darmawan.
+          </span>
+          <span className="font-firacode font-regular text-lg lg:text-xl lg:text-2xl text-slate-500">
+            I am a Software Engineer with a passion for building things that
+            help the world work.
+          </span>
+        </div>
+        <div className="hidden lg:flex">
+          <img
+            className="inline-block h-80 w-80 rounded-full"
+            src={ProfilePic}
+            alt=""
+          />
+        </div>
       </div>
       <div className="flex flex-row pt-4">
         <div className="mr-4">
